7de8b4dff06568bc7449d4a12cf9211e27795375
alberto <albertozanola1996@gmail.com>
2021-09-08 13:26:22 +0200

    Creazione della root

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 // Descrizione:
 // Rifare l'esercizio dello slider come fatto assieme in classe.
+const app = new Vue({
+    el: '#root'
+})
+
+
+
+
+
+
+
+
+
+
+
+
 // Bonus:
 // * Creare il navigatore in basso con i pallini bianchi (blu se corrispondente all'immagine attualmente visualizzata)
 // * Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
